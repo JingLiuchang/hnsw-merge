5f2a175467f592ce3eab72ce6f74537d120f438d
JingLiuchang <1287181291@qq.com>
2026-03-05 08:41:20 +0000

    test_Sim_merge

---
+++ examples/cpp/test_SIM_merge.cpp
@@ -0,0 +1,90 @@
+//
+// Created for SIM (SimJoin MST-guided Sliding Merge) algorithm testing
+//
+#include "../../hnswlib/hnswlib.h"
+#include <thread>
+#include "../../hnswlib/utils.h"
+#include "omp.h"
+#include "../../hnswlib/parameter.h"
+
+int main(int argc, char** argv) {
+    if (argc < 11 || argc > 13) {
+        std::cout << argv[0]
+                  << " data_file global_ef local_ef M sub_ef sub_M graph_num graph_index_file merged_nsg_path merge_order_selection [T] [merge_order_file]"
+                  << std::endl;
+        exit(-1);
+    }
+
+    float* data = NULL;
+    int max_elements, dim;
+    load_data(argv[1], data, max_elements, dim);
+    int ef_construction = atoi(argv[2]);
+    int local_ef = atoi(argv[3]);
+    int M = atoi(argv[4]);
+    int sub_ef = atoi(argv[5]);
+    int sub_M = atoi(argv[6]);
+    int graph_num = atoi(argv[7]);
+    std::string graph_index_file = std::string(argv[8]);
+    std::string merged_nsg_path = std::string(argv[9]);
+    std::string merge_order = std::string(argv[10]);
+    std::string merge_order_file = "None";
+    int T = 1;
+    if (argc == 12) {
+        T = atoi(argv[11]);
+    } else if (argc == 13) {
+        T = atoi(argv[11]);
+        merge_order_file = std::string(argv[12]);
+    }
+
+    hnswlib::Parameters params;
+    params.Set<int>("local_ef", local_ef);
+    params.Set<std::string>("method", "SIM");
+    params.Set<std::string>("merge_order_selection", merge_order);
+    params.Set<std::string>("merge_order_file", merge_order_file);
+    params.Set<bool>("print", true);
+
+    std::cout << "parameters: " << std::endl;
+    std::cout << "global_ef: " << ef_construction << std::endl;
+    std::cout << "local_ef: " << local_ef << std::endl;
+    std::cout << "M: " << M << std::endl;
+    std::cout << "sub_ef: " << sub_ef << std::endl;
+    std::cout << "sub_M: " << sub_M << std::endl;
+    std::cout << "graph_num: " << graph_num << std::endl;
+    std::cout << "graph_index_file: " << graph_index_file << std::endl;
+    std::cout << "merged_nsg_path: " << merged_nsg_path << std::endl;
+    std::cout << "merge_order_selection: " << merge_order << std::endl;
+    std::cout << "merge_order_file: " << merge_order_file << std::endl;
+    std::cout << "T: " << T << std::endl;
+    std::cout << std::endl;
+
+    // Initing index
+    hnswlib::L2Space space(dim);
+    hnswlib::MergeHierarchicalNSW<float>* alg_hnsw = new hnswlib::MergeHierarchicalNSW<float>(&space, max_elements, M, ef_construction);
+
+    std::vector<hnswlib::HierarchicalNSW<float>*> graphs(graph_num);
+    for (unsigned i = 0; i < graph_num; i++)
+    {
+        std::string index_file = graph_index_file + std::to_string(i+1) + "_ef" + std::to_string(sub_ef) + "_M" + std::to_string(sub_M) + ".hnsw";
+        hnswlib::L2Space space(dim);
+        hnswlib::HierarchicalNSW<float>* hnsw = new hnswlib::HierarchicalNSW<float>(&space);
+        hnsw->loadIndex(index_file, &space);
+        graphs[i] = hnsw;
+    }
+
+    int num_threads = T;
+    omp_set_num_threads(num_threads);
+
+    auto s = std::chrono::high_resolution_clock::now();
+    alg_hnsw->mgraph_merge(graph_num, graphs, params);
+    auto e = std::chrono::high_resolution_clock::now();
+
+    double merge_time = std::chrono::duration<double>(e - s).count();
+
+    std::cout << "Merge time: " << merge_time << " s; " << merged_nsg_path.substr(merged_nsg_path.find_last_of('/') + 1) << std::endl;
+
+    alg_hnsw->saveIndex(merged_nsg_path);
+
+    delete[] data;
+    delete alg_hnsw;
+    return 0;
+}
